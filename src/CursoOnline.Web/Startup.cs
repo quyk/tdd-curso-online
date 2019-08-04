6d6b3306f754fe8308fc87cdfdc72e9c3d3cc976
Quyk Mendonça <osilmar@outlook.com>
2019-08-04 00:32:41 -0300

    Seção 5 - Aula 27
    Mostrando mensagens de erro

--- src/CursoOnline.Web/Startup.cs
+++ src/CursoOnline.Web/Startup.cs
@@ -1,5 +1,6 @@
 ﻿using CursoOnline.Dominio._Base;
 using CursoOnline.IoC;
+using CursoOnline.Web.Filters;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -29,7 +30,10 @@ namespace CursoOnline.Web
             });
 
             StartupIoc.ConfigureService(services, Configuration);
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc(
+                config => config.Filters.Add(typeof(CustomExceptionFilter))
+                )
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
